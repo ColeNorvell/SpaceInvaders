99d9d88951f6b0a9751f27266a9e8cd46ffe83c0
ColeNorvell <42625940+ColeNorvell@users.noreply.github.com>
2018-08-22 18:43:54 -0500

    Update template.py

--- template.py
+++ template.py
@@ -7,24 +7,16 @@ screen = turtle.Screen()
 screen.screensize(500, 500, "black")
 
 # Create Sprite
-screen.addshape("sprite.gif")
+screen.addshape("Defender.gif")
 mySprite = turtle.Turtle()
 mySprite.hideturtle() # hide turtle until the the duck image replaces the placeholder shape
 mySprite.penup() # prevent sprite from drawing on screen as it moves
 mySprite.left(90) # rotate sprite for proper placement on screen
-mySprite.shape("sprite.gif") # Set Image for Sprite
-mySprite.setposition(0, 0)
+mySprite.shape("Defender.gif") # Set Image for Sprite
+mySprite.setposition(0,-200)
 mySprite.showturtle()
 
 # Create Player Controls
-def up():
-    global mySprite
-    mySprite.setposition(mySprite.xcor(), mySprite.ycor() + 10)
-
-def down():
-    global mySprite
-    mySprite.setposition(mySprite.xcor(), mySprite.ycor() - 10)
-
 def left():
     global mySprite
     mySprite.setposition(mySprite.xcor() - 10, mySprite.ycor())
@@ -37,8 +29,6 @@ def fire():
     print("FIRE! (space bar pressed)")
 
 # Set Event Listeners for Player Controls
-screen.onkey(up, "Up")
-screen.onkey(down, "Down")
 screen.onkey(left, "Left")
 screen.onkey(right, "Right")
 screen.onkey(fire, "space")
